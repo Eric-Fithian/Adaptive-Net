2ee7f5020f2699943bbb07761f2b05572b4507b1
Eric-Fithian <eric.fithian@icloud.com>
2026-01-05 17:08:45 -0600

    updated submit script to run all code

--- experiments/x3_openml/submit.sh
+++ experiments/x3_openml/submit.sh
@@ -17,10 +17,10 @@ source /project/caai_amzn_cmpr/efithian/Adaptive-Net/.venv/bin/activate
 cd /project/caai_amzn_cmpr/efithian/Adaptive-Net
 
 # 1. Data Collection (Run on Meta-Train datasets)
-# python -m experiments.x3_openml.s1_data_collection
+python -m experiments.x3_openml.s1_data_collection
 
 # 2. Train Policy
-# python -m experiments.x3_openml.s2_train_policy
+python -m experiments.x3_openml.s2_train_policy
 
 # 3. Meta-Test Evaluation
 python -m experiments.x3_openml.s3_evaluation
